68ecd1a2b7f84e728531cb298a22ae651f25eb6d
Hai Nguyen H.P <hai.nguyen3@asiantech.vn>
2020-06-30 09:00:16 +0700

    Setup init project

---
+++ FinalProjectAnhPham/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FinalProjectAnhPham
+//
+//  Created by MBA0321 on 6/30/20.
+//  Copyright © 2020 Asian Tech Co., Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'FinalProjectAnhPham' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for FinalProjectAnhPham
+  pod 'SwiftLint', '0.39.2'
+
+end
